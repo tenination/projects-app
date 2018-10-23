f5923ec744ccdac59baa993f323e654f7e7b3a2c
Tim Ninan <tninan@C02TH0ZXGTFM.attlocal.net>
2018-10-23 08:07:29 -0500

    Install React Router
    Basic setup of React Router.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
-class App extends Component {
+class App extends Component { 
   render() {
     return (
-      <div className="App">
-        <h1>Project Plan</h1>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <h1>Project Plan</h1>
+        </div>
+      </BrowserRouter>
     );
   }
 }

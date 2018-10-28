e129c6a1554ad2576e7b4068d4fc62f12e627c27
Tim Ninan <tninan@C02TH0ZXGTFM.attlocal.net>
2018-10-27 20:56:49 -0500

    Yup validation for Sign in page.
    Add Yup schema validation for login page.

--- client/src/components/auth/SignIn.js
+++ client/src/components/auth/SignIn.js
@@ -1,18 +1,25 @@
 import React from 'react'
 import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from "yup";
+
+const SignInSchema = Yup.object().shape({
+  email: Yup.string()
+    .email("Invalid email")
+    .required("Required"),
+  password: Yup.string()
+    .required("Required")
+});
+
+const ErrorMessageStyle = {
+  color: "red"
+};
 
 const SignIn = () => {
   return <div className="container">
       <h5 className="grey-text text-darken-3">Sign In</h5>
-      <Formik initialValues={{ email: "", password: "" }} validate={values => {
-          let errors = {};
-          if (!values.email) {
-            errors.email = "Required";
-          } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
-            errors.email = "Invalid email address";
-          }
-          return errors;
-        }} onSubmit={(values, { setSubmitting }) => {
+      <Formik initialValues={{ email: "", password: "" }} 
+       validationSchema={SignInSchema}
+       onSubmit={(values, { setSubmitting }) => {
           setTimeout(() => {
             alert(JSON.stringify(values, null, 2));
             setSubmitting(false);
@@ -22,12 +29,12 @@ const SignIn = () => {
           <div className="input-field">
             <label>Email</label>
             <Field type="email" name="email" />
-            <ErrorMessage name="email" component="div" />
+          <ErrorMessage style={ErrorMessageStyle} name="email" component="div" />
           </div>
           <div className="input-field">
             <label>Password</label>
             <Field type="password" name="password" />
-            <ErrorMessage name="password" component="div" />
+          <ErrorMessage style={ErrorMessageStyle} name="password" component="div" />
           </div>
             <button 
               className="btn pink lighten-1 z-depth-0" 

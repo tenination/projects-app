a5dd4b639d6afff771354b0e2f4ccbb9e5d7ef2d
Tim Ninan <tninan@C02TH0ZXGTFM.attlocal.net>
2018-10-28 18:57:16 -0500

    Setup Redux Thunk
    Made createProject action creator to handle async actions.

--- client/src/components/projects/CreateProject.js
+++ client/src/components/projects/CreateProject.js
@@ -1,44 +1,74 @@
-import React from 'react'
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import React from "react";
+import { connect } from "react-redux";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
+import { createProject } from "../../store/actions/projectActions";
 
 const CreateProjectSchema = Yup.object().shape({
-  title: Yup.string()
-    .required("Required"),
-  content: Yup.string()
-    .required("Required")
+  title: Yup.string().required("Required"),
+  content: Yup.string().required("Required")
 });
 
 const ErrorMessageStyle = {
   color: "red"
 };
 
-const CreateProject = () => {
-  return <div className="container">
+const CreateProject = props => {
+  return (
+    <div className="container">
       <h5 className="grey-text text-darken-3">Create new project</h5>
-      <Formik initialValues={{ title: "", content: "" }} validationSchema={CreateProjectSchema} onSubmit={(values, { setSubmitting }) => {
+      <Formik
+        initialValues={{ title: "", content: "" }}
+        validationSchema={CreateProjectSchema}
+        onSubmit={(values, { setSubmitting }) => {
           setTimeout(() => {
             alert(JSON.stringify(values, null, 2));
+            props.createProject(values);
             setSubmitting(false);
           }, 400);
-        }}>
-        {({ isSubmitting }) => <Form>
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
             <div className="input-field">
               <label>Title</label>
               <Field type="text" name="title" />
-              <ErrorMessage style={ErrorMessageStyle} name="title" component="div" />
+              <ErrorMessage
+                style={ErrorMessageStyle}
+                name="title"
+                component="div"
+              />
             </div>
             <div className="input-field">
               <label>Project Content</label>
               <Field type="text" name="content" />
-              <ErrorMessage style={ErrorMessageStyle} name="content" component="div" />
+              <ErrorMessage
+                style={ErrorMessageStyle}
+                name="content"
+                component="div"
+              />
             </div>
-            <button className="btn pink lighten-1 z-depth-0" disabled={isSubmitting}>
+            <button
+              className="btn pink lighten-1 z-depth-0"
+              disabled={isSubmitting}
+              type="submit"
+            >
               CREATE
             </button>
-          </Form>}
+          </Form>
+        )}
       </Formik>
-    </div>;
-}
+    </div>
+  );
+};
 
-export default CreateProject;
+const mapDispatchToProps = dispatch => {
+  return {
+    createProject: project => dispatch(createProject(project))
+  };
+};
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(CreateProject);
--- client/src/store/reducers/projectReducer.js
+++ client/src/store/reducers/projectReducer.js
@@ -44,6 +44,14 @@ const initState = {
 };
 
 const projectReducer = (state = initState, action) => {
+  switch (action.type) {
+    case "CREATE_PROJECT":
+      console.log("created project", action.project);
+      break;
+    default:
+      console.log("this is the default case");
+      break;
+  }
   return state;
 };
 

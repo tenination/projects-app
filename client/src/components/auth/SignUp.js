d5691bb33b8eb93ab4442e5cbbd14f0f88229857
Tim Ninan <tninan@C02TH0ZXGTFM.attlocal.net>
2018-10-27 20:51:49 -0500

    Implement Yup
    Added Yup validation schema for sign up page.

--- client/src/components/auth/SignUp.js
+++ client/src/components/auth/SignUp.js
@@ -1,5 +1,24 @@
 import React from 'react'
 import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+
+const SignupSchema = Yup.object().shape({
+  email: Yup.string()
+    .email("Invalid email")
+    .required("Required"),
+  password: Yup.string()
+    .min(8, "Too Short!")
+    .max(20, "Too Long!")
+    .required("Required"),
+  firstName: Yup.string()
+    .min(2, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+  lastName: Yup.string()
+    .min(2, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required")
+});
 
 const ErrorMessageStyle = {
   color: 'red'
@@ -8,15 +27,9 @@ const ErrorMessageStyle = {
 const SignUp = () => {
   return <div className="container">
       <h5 className="grey-text text-darken-3">Sign Up</h5>
-      <Formik initialValues={{ email: "", password: "" }} validate={values => {
-          let errors = {};
-          if (!values.email) {
-            errors.email = "Required";
-          } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
-            errors.email = "Invalid email address";
-          }
-          return errors;
-        }} onSubmit={(values, { setSubmitting }) => {
+      <Formik initialValues={{ email: "", password: "" }}
+        validationSchema={SignupSchema}
+        onSubmit={(values, { setSubmitting }) => {
           setTimeout(() => {
             alert(JSON.stringify(values, null, 2));
             setSubmitting(false);
@@ -31,19 +44,19 @@ const SignUp = () => {
             <div className="input-field">
               <label>Password</label>
               <Field type="password" name="password" />
-              <ErrorMessage name="password" component="div" />
+              <ErrorMessage style={ErrorMessageStyle} name="password" component="div" />
             </div>
             <div className="input-field">
               <label>First Name</label>
-              <Field type="text" name="first-name" />
-              <ErrorMessage name="first-name" component="div" />
+              <Field type="text" name="firstName" />
+              <ErrorMessage style={ErrorMessageStyle} name="firstName" component="div" />
             </div>
             <div className="input-field">
               <label>Last Name</label>
-              <Field type="text" name="last-name" />
-              <ErrorMessage name="last-name" component="div" />
+              <Field type="text" name="lastName" />
+              <ErrorMessage style={ErrorMessageStyle} name="lastName" component="div" />
             </div>
-            <button className="btn pink lighten-1 z-depth-0" disabled={isSubmitting}>
+            <button type="submit" className="btn pink lighten-1 z-depth-0" disabled={isSubmitting}>
               Sign Up
             </button>
           </Form>}

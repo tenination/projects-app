a83e9fac5fd4ad0e1a6fde9448a6aff243f977cb
Tim Ninan <tninan@C02TH0ZXGTFM.attlocal.net>
2018-11-10 17:27:53 -0600

    Implement project add with firebase
    CREATE_PROJECT action now saves to firebase

--- client/src/store/reducers/projectReducer.js
+++ client/src/store/reducers/projectReducer.js
@@ -2,57 +2,58 @@ const initState = {
   projects: [
     {
       id: 1,
-      name: "Shibuya Crossing",
+      name: 'Shibuya Crossing',
       content:
-        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic a non commodi illum consectetur nesciunt, neque, nostrum vitae delectus perferendis quidem pariatur autem, totam vero et quos ullam ipsa ab",
-      author: "Yoshi",
-      date: "Oct 27"
+        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic a non commodi illum consectetur nesciunt, neque, nostrum vitae delectus perferendis quidem pariatur autem, totam vero et quos ullam ipsa ab',
+      author: 'Yoshi',
+      date: 'Oct 27',
     },
     {
       id: 2,
-      name: "Ginza Shopping",
+      name: 'Ginza Shopping',
       content:
-        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic a non commodi illum consectetur nesciunt, neque, nostrum vitae delectus perferendis quidem pariatur autem, totam vero et quos ullam ipsa ab",
-      author: "Takayoki",
-      date: "Oct 25"
+        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic a non commodi illum consectetur nesciunt, neque, nostrum vitae delectus perferendis quidem pariatur autem, totam vero et quos ullam ipsa ab',
+      author: 'Takayoki',
+      date: 'Oct 25',
     },
     {
       id: 3,
-      name: "Harajuku Fashion",
+      name: 'Harajuku Fashion',
       content:
-        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic a non commodi illum consectetur nesciunt, neque, nostrum vitae delectus perferendis quidem pariatur autem, totam vero et quos ullam ipsa ab",
-      author: "Kata",
-      date: "Oct 21"
+        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic a non commodi illum consectetur nesciunt, neque, nostrum vitae delectus perferendis quidem pariatur autem, totam vero et quos ullam ipsa ab',
+      author: 'Kata',
+      date: 'Oct 21',
     },
     {
       id: 4,
-      name: "Shinjuku District",
+      name: 'Shinjuku District',
       content:
-        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic a non commodi illum consectetur nesciunt, neque, nostrum vitae delectus perferendis quidem pariatur autem, totam vero et quos ullam ipsa ab",
-      author: "Yokohama",
-      date: "Oct 20"
+        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic a non commodi illum consectetur nesciunt, neque, nostrum vitae delectus perferendis quidem pariatur autem, totam vero et quos ullam ipsa ab',
+      author: 'Yokohama',
+      date: 'Oct 20',
     },
     {
       id: 5,
-      name: "Asakusa Temple",
+      name: 'Asakusa Temple',
       content:
-        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic a non commodi illum consectetur nesciunt, neque, nostrum vitae delectus perferendis quidem pariatur autem, totam vero et quos ullam ipsa ab",
-      author: "Roku",
-      date: "Oct 14"
-    }
-  ]
+        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic a non commodi illum consectetur nesciunt, neque, nostrum vitae delectus perferendis quidem pariatur autem, totam vero et quos ullam ipsa ab',
+      author: 'Roku',
+      date: 'Oct 14',
+    },
+  ],
 };
 
 const projectReducer = (state = initState, action) => {
   switch (action.type) {
-    case "CREATE_PROJECT":
-      console.log("created project", action.project);
-      break;
+    case 'CREATE_PROJECT':
+      console.log('created project', action.project);
+      return state;
+    case 'CREATE_PROJECT_ERROR':
+      console.log('create project error', action.err);
+      return state;
     default:
-      console.log("this is the default case");
-      break;
+      return state;
   }
-  return state;
 };
 
 export default projectReducer;

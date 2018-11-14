95ef32417c67bdf073054fbb238ce628179bf8dd
Tim Ninan <tninan@C02TH0ZXGTFM.local>
2018-11-14 16:06:39 -0600

    Make nav links conditional
    The correct nav links should show up based
    on whether user is logged in or logged out.

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -4,15 +4,16 @@ import SignedInLinks from './SignedInLinks';
 import SignedOutLinks from './SignedOutLinks';
 import { connect } from 'react-redux';
 
-const Navbar = () => {
+const Navbar = props => {
+  const { auth } = props;
+  const links = auth.uid ? <SignedInLinks /> : <SignedOutLinks />;
   return (
     <nav className="nav-wrapper grey darken-3">
       <div className="container">
         <Link to="/" className="brand-logo">
           ProjectPlan
         </Link>
-        <SignedInLinks />
-        <SignedOutLinks />
+        {links}
       </div>
     </nav>
   );
@@ -20,7 +21,9 @@ const Navbar = () => {
 
 const mapStateToProps = state => {
   console.log(state);
-  return {};
+  return {
+    auth: state.firebase.auth,
+  };
 };
 
 export default connect(mapStateToProps)(Navbar);
